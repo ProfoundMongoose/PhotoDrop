94d41f20d2b29442d0dd533fc29ab9b4198ced9f
Max Kroshka <max.kroshka@gmail.com>
2016-02-26 21:56:49 -0800

    remove console logs

--- server/photos/photoController.js
+++ server/photos/photoController.js
@@ -6,41 +6,36 @@ module.exports = {
   // recieve base64 bit image in two POST request packets
   // send that file to imgur
   uploadPhoto: (function() {
-      var currentPipes= {
-        // key pairs are the userId and the data.
-        userId: 'data'
-      }
-
-      return function (req, res, next) {
+    var currentPipes = {
+      // key pairs are the userId and the data.
+      userId: 'data'
+    }
+    return function(req, res, next) {
       // check if it is first or second packet
       var userId = req.body.userId;
       if (currentPipes[userId] === undefined) {
         currentPipes[userId] = req.body.data;
-        console.log(1)
         // if the userId is there, we know that the photo is halfway upladed
       } else if (currentPipes[userId]) {
-        console.log(2)
         var fullImgData = currentPipes[userId] + req.body.data;
         // clear out userId data space for future images
         currentPipes[userId] = undefined;
-        console.log(fullImgData)
         imgur.uploadBase64(fullImgData)
-        .then(function (json) {
-          console.log(json.data.link);
-          req.imgurLink = json.data.link;
-          next();
-        })
-        .catch(function (err) {
-          console.error(err.message);
-        });
+          .then(function(json) {
+            console.log(json.data.link);
+            req.imgurLink = json.data.link;
+            next();
+          })
+          .catch(function(err) {
+            console.error(err.message);
+          });
       }
 
-      }
-    })(),
+    }
+  })(),
 
   // save that photo as  a model in db
-  savePhotoModelToDB: function (req, res, next) {
-    console.log(req.body.userId);
+  savePhotoModelToDB: function(req, res, next) {
     new Photo({
       url: req.imgurLink,
       loc: {
@@ -73,8 +68,7 @@ module.exports = {
         }
       }
     }, function(err, photos) {
-      if (err) { next(error); }
-      console.log('fetched photos', photos);
+      if (err) next(error); 
       res.json(photos);
     });
   },
@@ -108,7 +102,6 @@ module.exports = {
       }
     }, function(err, photos) {
       if (err) next(error);
-      console.log('photos after first find', photos);
       revealedPhotos = photos;
       Photo.find({
         loc: {
@@ -119,14 +112,13 @@ module.exports = {
             }
           }
         },
-        _id: { $nin: revealedPhotos.map(function(photo) {
-            return photo._id }) }
-      }, 'loc', function(err, photos) {
-        console.log('photos outside of circle', photos);
-        if (err) {
-          console.log('error: ', error);
-          next(error);
+        _id: {
+          $nin: revealedPhotos.map(function(photo) {
+            return photo._id
+          })
         }
+      }, 'loc', function(err, photos) {
+        if (err) next(error);
         res.json(photos);
       });
     })
