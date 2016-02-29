1194a410556b2766c4a09f9137cb03e850ae5785
Evan Chen <evanwchen@gmail.com>
2016-02-29 09:33:19 -0800

    Add Mocha ServerSpec file. Update package.json to require supertest and chai.

---
+++ test/SeverSpec.js
@@ -0,0 +1,21 @@
+var request = require('supertest');
+var express = require('express');
+var expect = require('chai').expect;
+var app = require('../server/server.js').app;
+var db = require('../server/server.js').db;
+
+describe('', function() {
+
+  beforeEach(function(done) {
+    
+    done();
+  });
+
+  describe('ABC: ', function() {
+
+  };
+
+  describe('XYZ: ', function() {
+
+  };
+};
