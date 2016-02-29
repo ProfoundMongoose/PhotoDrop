bdc1d68858114ae1540474f042632fe18f51994e
Evan Chen <evanwchen@gmail.com>
2016-02-28 18:33:06 -0800

    Add .eslintrc configs with linting rules. Update package.json with relevant ES6/React/React Native eslint modules.
    Included a set of supported rules from here:
    https://github.com/yannickcr/eslint-plugin-react#user-content-recommended-configuration
    https://github.com/Intellicode/eslint-plugin-react-native

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,68 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    // nodemon: {
+    //   dev: {
+    //     script: 'server.js'
+    //   }
+    // },
+
+    // uglify: {
+    //   my_target: {
+    //     files: {
+    //       './public/dist/builtUglify.js': ['./public/dist/built.js'],
+    //     }
+    //   }
+    // },
+
+    // eslint: {
+    //   target: [
+    //     'server/*.js', 'server/config/*.js', 'server/photos/*.js', 'server/users/*.js',
+    //     'client/App/Components/*.js', 'client/App/Utils/*.js'
+    //   ]
+    // },
+
+    shell: {
+      prodServer: {
+        command: 'git push live master'
+      },
+      eslint: {
+        command: 'eslint client/App/Components/Camera.js'
+      }
+    }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-eslint');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+  // grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-nodemon');
+  // grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('test', ['mochaTest']);
+
+  grunt.registerTask('build', ['uglify']);
+
+  grunt.registerTask('lint', ['shell:eslint']);
+
+  grunt.registerTask('upload', function(n) {
+    if (grunt.option('prod')) {
+      // add your production server task here
+      grunt.task.run([ 'shell' ])
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', ['eslint', 'mochaTest', 'upload']);
 
 };
