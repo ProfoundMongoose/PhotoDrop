0b3040213357547524a3b532dbadf1490f4435ab
Max Kroshka <max.kroshka@gmail.com>
2016-02-29 14:36:57 -0800

    Don't hash password if it has not changed

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -39,6 +39,10 @@ UserSchema.methods.comparePasswords = function (candidatePassword) {
 UserSchema.pre('save', function (next) {
   var user = this;
 
+  if (!user.isModified('password')) {
+    return next();
+  }
+  
   bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
     if (err) {
       return next(err);
