31314495f0ca1a6f00adcf7202f150b65b4b4b71
Evan Chen <evanwchen@gmail.com>
2016-02-28 19:06:56 -0800

    Update package.json with grunt/mocha dev-dependencies. Add test, upload, deploy tasks to Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,59 +12,39 @@ module.exports = function(grunt) {
       }
     },
 
-    // nodemon: {
-    //   dev: {
-    //     script: 'server.js'
-    //   }
-    // },
-
-    // uglify: {
-    //   my_target: {
-    //     files: {
-    //       './public/dist/builtUglify.js': ['./public/dist/built.js'],
-    //     }
-    //   }
-    // },
-
-    // eslint: {
-    //   target: [
-    //     'server/*.js', 'server/config/*.js', 'server/photos/*.js', 'server/users/*.js',
-    //     'client/App/Components/*.js', 'client/App/Utils/*.js'
-    //   ]
-    // },
+    nodemon: {
+      dev: {
+        script: 'server/server.js'
+      }
+    },
 
     shell: {
       prodServer: {
-        command: 'git push live master'
+        command: 'git push live master',
+        options: {
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
       },
       eslint: {
-        command: 'eslint client/App/Components/Camera.js'
+        command: 'eslint client/App/*/*.js server/*/*.js'
       }
     }
   });
 
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-watch');
-  // grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
-  // grunt.loadNpmTasks('grunt-nodemon');
-
-  grunt.registerTask('test', ['mochaTest']);
-
-  grunt.registerTask('build', ['uglify']);
+  grunt.loadNpmTasks('grunt-nodemon');
 
   grunt.registerTask('lint', ['shell:eslint']);
 
+  grunt.registerTask('test', ['mochaTest', 'lint']);
+
   grunt.registerTask('upload', function(n) {
-    if (grunt.option('prod')) {
-      // add your production server task here
-      grunt.task.run([ 'shell' ])
-    } else {
-      grunt.task.run([ 'server-dev' ]);
-    }
+    grunt.task.run([ 'shell:prodServer' ]);
   });
 
-  grunt.registerTask('deploy', ['eslint', 'mochaTest', 'upload']);
+  grunt.registerTask('deploy', ['lint', 'mochaTest', 'upload']);
 
 };
