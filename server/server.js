6ae90cdba0e5835457b71d56c39e5a2c05b560de
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-03-01 12:28:51 -0800

    Clean up code and add 'morgan' logging middleware

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var app = express();
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-// reqire bodyparser middleware for dealing with POST requests
-var bodyParser = require('body-parser');
+var portNum = 8000;
+
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/photodrop');
+mongoose.connect('mongodb://localhost/photodrop');
 var db = mongoose.connection;
 
 require('./config/routes.js')(app, express);
 
-app.listen(8000);
-console.log('Starting server. Listening on 8000');
+app.listen(portNum, function () {
+  console.log('Server started. Listening on port ' + portNum);
+});
 
 module.exports = {
   app: app,
