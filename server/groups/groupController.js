83fbc60f5a40790e5fe430e22d3f2ddf70c2e7e2
Kyle Corbelli <kyle.corbelli@gmail.com>
2016-03-03 12:47:38 -0800

    Develop/improve route/handler for joining a group

--- server/groups/groupController.js
+++ server/groups/groupController.js
@@ -7,26 +7,43 @@ module.exports = {
   searchGroups: function (req, res, next) {
     var regexSearch = new RegExp(req.params.groupname, 'i');
     Group.find({groupname: regexSearch}, {_id: 1, groupname: 1, description: 1}, function (err, groups) {
-      console.log('called');
       if (err) {
         next(err);
+      } else {
+        res.json(groups);
       }
-      res.json(groups);
     });
   },
 
   addGroup: function (req, res, next) {
-    User.findOne({_id: mongoose.mongo.ObjectID(req.body.currentUserId)}, {_id: 1, username: 1}, function (err, currentUser) {
+    User.findOne({_id: mongoose.mongo.ObjectID(req.body.currentUserId)}, {_id: 1, username: 1, groups: 1}, function (err, currentUser) {
       var group = new Group({
         groupname: req.body.groupname,
         description: req.body.description,
-        administrator: currentUser.username
+        administrator: currentUser.username,
+        members: [
+          {
+            _id: mongoose.mongo.ObjectID(currentUser._id),
+            username: currentUser.username
+          }
+        ]
       });
       group.save(function (err) {
         if (err) {
-          next(err);
+          return next(err);
+        } else {
+          currentUser.groups.addToSet({
+            _id: mongoose.mongo.ObjectID(group._id),
+            groupname: group.groupname
+          });
+          currentUser.save(function (err) {
+            if (err) {
+              return next(err);
+            } else {
+              res.sendStatus(201);
+            }
+          });
         }
-        res.sendStatus(201);
       });
     });
   },
@@ -34,29 +51,32 @@ module.exports = {
   joinGroup: function (req, res, next) {
     User.findOne({_id: mongoose.mongo.ObjectID(req.body.currentUserId)}, {_id: 1, username: 1, groups: 1}, function (err, currentUser) {
       if (err) {
-        next(err);
-      }
-      Group.findOne({groupname: req.body.targetGroupname}, {_id: 1, groupname: 1, members: 1}, function (err, targetGroup) {
-        targetGroup.members.addToSet({
-          _id: mongoose.mongo.ObjectID(currentUser._id),
-          username: currentUser.username
-        });
-        targetGroup.save(function (err) {
-          if (err) {
-            next(err);
-          }
-          currentUser.groups.addToSet({
-            _id: mongoose.mongo.ObjectID(targetGroup._id),
-            groupname: targetGroup.groupname
+        return next(err);
+      } else {
+        Group.findOne({groupname: req.body.targetGroupname}, {_id: 1, groupname: 1, members: 1}, function (err, targetGroup) {
+          targetGroup.members.addToSet({
+            _id: mongoose.mongo.ObjectID(currentUser._id),
+            username: currentUser.username
           });
-          currentUser.save(function (err) {
+          targetGroup.save(function (err) {
             if (err) {
-              next(err);
+              return next(err);
+            } else {
+              currentUser.groups.addToSet({
+                _id: mongoose.mongo.ObjectID(targetGroup._id),
+                groupname: targetGroup.groupname
+              });
+              currentUser.save(function (err) {
+                if (err) {
+                  return next(err);
+                } else {
+                  res.sendStatus(201);
+                }
+              });
             }
-            res.sendStatus(201);
           });
         });
-      });
+      }
     });
   }
 
