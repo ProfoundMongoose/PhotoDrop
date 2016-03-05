b2377d4c57098b22654e697f14d7c8b15180cb64
Erick Paepke <epaepke@gmail.com>
2016-03-04 17:39:05 -0800

    Update groups view to handle access from map view and settings

--- client/App/Components/GroupsList.js
+++ client/App/Components/GroupsList.js
@@ -51,13 +51,15 @@ class GroupsList extends React.Component {
     });
   }
 
-  // onPress={()=>this.props.route.addGroupFilter(group)}
-
   renderGroup(group) {
     return (
       <View style={styles.container}>
         <View style={styles.rightContainer}>
-          <Text style={styles.group} onPress={()=>this.props.route.addGroupFilter(group, this.props.navigator)}>{group.groupname}</Text>
+          <Text style={styles.group} onPress={()=>{
+            if (this.props.route.addGroupFilter) {
+              this.props.route.addGroupFilter(group, this.props.navigator)
+            }
+          }}>{group.groupname}</Text>
         </View>
         <View style={styles.rightContainer}>
           <Text style={styles.number}>{group.members.length + ' Users'}</Text>
